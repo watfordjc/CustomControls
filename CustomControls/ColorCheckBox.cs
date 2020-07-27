9dde1100b9817a68590fd3162c4129b7d52c48b1
John Cook <coder-a599-7927@johncook.co.uk>
2020-07-27 15:31:08 +0100

    Define default brush colours

--- CustomControls/ColorCheckBox.cs
+++ CustomControls/ColorCheckBox.cs
@@ -13,37 +13,21 @@ using System.Windows.Shapes;
 
 namespace CustomControls
 {
-    /// <summary>
-    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
-    ///
-    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:CustomControls"
-    ///
-    ///
-    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:CustomControls;assembly=CustomControls"
-    ///
-    /// You will also need to add a project reference from the project where the XAML file lives
-    /// to this project and Rebuild to avoid compilation errors:
-    ///
-    ///     Right click on the target project in the Solution Explorer and
-    ///     "Add Reference"->"Projects"->[Browse to and select this project]
-    ///
-    ///
-    /// Step 2)
-    /// Go ahead and use your control in the XAML file.
-    ///
-    ///     <MyNamespace:CustomControl1/>
-    ///
-    /// </summary>
     public class ColorCheckBox : CheckBox
     {
+        private static readonly Brush BackgroundBrush = new SolidColorBrush(Color.FromArgb(0xFF, 0xFF, 0xFF, 0xFF));
+        private static readonly Brush GlyphBrush = new SolidColorBrush(Color.FromArgb(0xFF, 0x21, 0x21, 0x21));
+        private static readonly Brush DefaultBorderBrush = new SolidColorBrush(Color.FromArgb(0xFF, 0x70, 0x70, 0x70));
+        private static readonly Brush MouseOverBackgroundBrush = new SolidColorBrush(Color.FromArgb(0xFF, 0xF3, 0xF9, 0xFF));
+        private static readonly Brush MouseOverBorderBrush = new SolidColorBrush(Color.FromArgb(0xFF, 0x55, 0x93, 0xFF));
+        private static readonly Brush MouseOverGlyphBrush = new SolidColorBrush(Color.FromArgb(0xFF, 0x21, 0x21, 0x21));
+        private static readonly Brush PressedBackgroundBrush = new SolidColorBrush(Color.FromArgb(0xFF, 0xD9, 0xEC, 0xFF));
+        private static readonly Brush PressedBorderBrush = new SolidColorBrush(Color.FromArgb(0xFF, 0x3C, 0x77, 0xDD));
+        private static readonly Brush PressedGlyphBrush = new SolidColorBrush(Color.FromArgb(0xFF, 0x21, 0x21, 0x21));
+        private static readonly Brush DisabledBackgroundBrush = new SolidColorBrush(Color.FromArgb(0xFF, 0xE6, 0xE6, 0xE6));
+        private static readonly Brush DisabledBorderBrush = new SolidColorBrush(Color.FromArgb(0xFF, 0xBC, 0xBC, 0xBC));
+        private static readonly Brush DisabledGlyphBrush = new SolidColorBrush(Color.FromArgb(0xFF, 0x70, 0x70, 0x70));
+
         static ColorCheckBox()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorCheckBox), new FrameworkPropertyMetadata(typeof(ColorCheckBox)));
